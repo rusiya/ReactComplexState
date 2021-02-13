64bfd2901c0ce47e5c7a5d52986203a74d1bf6bc
rusiya <41211499+rusiya@users.noreply.github.com>
2021-02-13 10:20:33 -0800

    Spread Operator Examples

--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// const abc = ["a", "b", "c"];
+// const mno = ["m", "n", "o", ...abc]; // ... is the Spread Operator.
+// console.log(abc);
+// console.log(mno);
+
+// const fullName = {
+//   fName: "Subhash",
+//   lName: "Rusiya"
+// };
+
+// const userRight = {
+//   ...fullName, //This will spread
+//   id: 1
+// };
+
+// const userWrong = {
+//   fullName,
+//   id: 1
+// };
+
+// console.log(fullName);
+// console.log(userRight);
+// console.log(userWrong);
